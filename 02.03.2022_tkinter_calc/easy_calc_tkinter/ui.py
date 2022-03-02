c803e786cc57050948d21a86741fda6ae610925a
user123 <dog@mail.com>
2022-03-02 22:11:05 +0300

    Создал логику для калькулятора

--- 02.03.2022_tkinter_calc/easy_calc_tkinter/ui.py
+++ 02.03.2022_tkinter_calc/easy_calc_tkinter/ui.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+from . import core
+
+
 class EasyCalcFrame(tk.Frame):
     def __init__(self,*args,**kwargs):
         super(EasyCalcFrame, self).__init__(*args,**kwargs)
 
-        self.output = tk.Entry(self,state=tk.DISABLED)
+        self.output_text = tk.StringVar()
+        self.output = tk.Entry(self,state=tk.DISABLED, textvariable=self.output_text)
         self.output.grid(columnspan=4,row=0)
 
         lbl_left_number = tk.Label(self,text="Первое число")
@@ -12,23 +16,61 @@ class EasyCalcFrame(tk.Frame):
         lbl_right_number = tk.Label(self, text="Второе число")
         lbl_right_number.grid(column=2,columnspan=2,row=1)
 
-        self.first_entry = tk.Entry(self)
+        self.first_entry_text=tk.StringVar()
+        self.first_entry = tk.Entry(self,textvariable=self.first_entry_text)
         self.first_entry.grid(columnspan=2, row=2)
 
-        self.second_entry = tk.Entry(self)
+        self.second_entry_text = tk.StringVar()
+        self.second_entry = tk.Entry(self,textvariable=self.second_entry_text)
         self.second_entry.grid(column=2,columnspan=2, row=2)
 
-        lbl_sum = tk.Button(self, text="+")
-        lbl_sum.grid(row=3)
+        btn_add = tk.Button(self, text="+")
+        btn_add.grid(row=3)
 
-        lbl_minus = tk.Button(self, text="-")
-        lbl_minus.grid(column=1, row=3)
+        btn_sub = tk.Button(self, text="-")
+        btn_sub.grid(column=1, row=3)
 
-        lbl_inc = tk.Button(self, text="*")
-        lbl_inc.grid(column=2, row=3)
+        btn_mul = tk.Button(self, text="*")
+        btn_mul.grid(column=2, row=3)
 
-        lbl_div = tk.Button(self, text="/")
-        lbl_div.grid(column=3, row=3)
+        btn_div = tk.Button(self, text="/")
+        btn_div.grid(column=3, row=3)
+
+        def action(left_value,right_value,op):
+            result = op(left_value,right_value)
+            self.output_text.set(result)
+            self.first_entry_text.set("")
+            self.second_entry_text.set("")
+
+        btn_add.configure(
+            command=lambda:action(
+            self.first_entry_text.get(),
+            self.second_entry_text.get(),
+            core.do_addition
+            )
+        )
+        btn_sub.configure(
+            command=lambda: action(
+                self.first_entry_text.get(),
+                self.second_entry_text.get(),
+                core.do_subtraction
+            )
+        )
+        btn_mul.configure(
+            command=lambda: action(
+                self.first_entry_text.get(),
+                self.second_entry_text.get(),
+                core.do_mul
+            )
+        )
+        btn_div.configure(
+            command=lambda: action(
+                self.first_entry_text.get(),
+                self.second_entry_text.get(),
+                core.do_div
+            )
+        )
+
 
 
 class App(tk.Tk):
