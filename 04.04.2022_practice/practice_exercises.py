e01393705428588a4a1ca6a61ffdac3dbcb50cc2
user123 <dog@mail.com>
2022-04-04 21:57:08 +0300

    Повторение тем(практика)

---
+++ 04.04.2022_practice/practice_exercises.py
@@ -0,0 +1,45 @@
+# #1задача
+# x = input("Введите число")
+# total=0
+# for i in x:
+#      if i == "8":
+#          total +=2
+#      if i == "0":
+#          total += 1
+#      if i == "6":
+#         total += 1
+#      if i == "9":
+#          total += 1
+# print(total)
+#2задача
+import math
+# place = int(input("Введите номер места"))
+# print(f'Номер вашего купе:{math.ceil(place//4)}')
+
+# 3задача
+# start_series = "AB01643"
+# start_series = int(start_series[2::])
+# num = input("Введите серию вашего билета без пробела")
+# num = int(num[2::])
+# line = math.ceil((num - start_series)/15)
+
+# print(f"Согласно введеному серийнику билета ваш ряд:{line}")
+#4задача
+# n = int(input("Введите число"))
+# row = math.ceil(n/5)
+# column = int(n/row)
+# print(f'Строка:{row}')
+# print(f'Столбец:{column}')
+
+#5задача
+#1Вариант
+# case = int(input("Введите номер ячейки"))
+# stage = math.ceil(case/120)
+# section = math.ceil((case/15))
+# print(f'Ярус:{stage}')
+# print(f'Секция:{section}')
+#2Вариант
+# stage2 = math.ceil()
+# section2 = math.ceil(case/141)
+# print(f'Секция:{section2}')
+#
